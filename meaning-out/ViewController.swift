5fd941d5dbf802a3c786852ef94875d205e18a41
dev-junehee <dev.junehee@gmail.com>
2024-06-13 18:45:10 +0900

    Initial Commit

---
+++ meaning-out/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meaning-out
+//
+//  Created by junehee on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

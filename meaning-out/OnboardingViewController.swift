ef5f27bb3ada0f18df610f26f1092173780f655b
dev-junehee <dev.junehee@gmail.com>
2024-06-14 00:42:29 +0900

    Feat: 스플래시 화면 구현, 온보딩 화면 연결

---
+++ meaning-out/OnboardingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingViewController.swift
+//  meaning-out
+//
+//  Created by junehee on 6/14/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        print("온보딩 화면 진입")
+        
+    }
+
+}

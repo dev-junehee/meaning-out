b1a6c94956ab07d5726f780fa752f117f9f4db2d
dev-junehee <dev.junehee@gmail.com>
2024-06-15 02:13:41 +0900

    Feat: 프로필 닉네임 수정 화면 - 저장 BarButton 추가

--- meaning-out/Controllers/Setting/EditNicknameViewController.swift
+++ meaning-out/Controllers/Setting/EditNicknameViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 
 class EditNicknameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureView()
     }
     
     func configureView() {
         view.backgroundColor = Resource.Colors.white
         navigationItem.title = "EDIT PROFILE"
+        
+        addImgBarBtn(image: Resource.SystemImages.left, style: .plain, target: self, action: #selector(backButtonClicked), type: .left)
+        addTextBarBtn(title: Constants.Text.Button.save.rawValue, style: .plain, target: self, action: #selector(saveButtonClicked), type: .right)
     }
-
-
+    
+    
+    @objc func backButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func saveButtonClicked() {
+        print("저장 버튼 눌렀어요")
+    }
 }
